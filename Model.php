d2b11a8b2171c3ca44c74ca95dfd5adcc55bbb2d
gogabidzia <gogabidzia@gmail.com>
2017-07-07 21:59:59 +0400

    __get method for relationships

--- Model.php
+++ Model.php
@@ -25,23 +25,33 @@ class Model{
 		}
 	}
 
-	public function defaultTableName(){
+	public function __get($key){
+		return $this->getAttribute($key);
+	}
+	public function getAttribute($key){
+		if (! $key) {
+        	return;
+	    }
+	    if(array_key_exists($key, get_object_vars($this))){
+			return;	    	
+	    }
+	    if(method_exists($this, $key)){
+	    	// $this->$key = $key;
+	    	// return;
+	    	return $this->$key();
+	    }
+	}
+
+	private function defaultTableName(){
 		return strtolower(get_class(self)) . "s";
 	}
-	public function save(){
-
-	}
-	public function getCallingClass(){
+	private function getCallingClass(){
 		$className = get_called_class();
 		return new $className();
 	}
-	public function getThis(){
-		return $this;
-	}
 	public function all(){
-		$instance = self::getCallingClass();
-		$instance->query = "SELECT * FROM " . $instance->table;
-		return $instance;
+		$instance = isset($this)?$this:self::getCallingClass();
+		return $instance->get();
 	}
 	public function where($field,$operator,$value=null){
 		$instance = isset($this)?$this:self::getCallingClass();
@@ -54,6 +64,9 @@ class Model{
 		$operator . " '" . $value."' ";
 		return $instance;
 	}
+	public function save(){
+
+	}
 	public function orderBy($field, $ascdesc){
 		$instance = isset($this)?$this:self::getCallingClass();
 		$instance->query.= "ORDER BY " . $field . " " . $ascdesc;
@@ -62,23 +75,6 @@ class Model{
 	public function limit($limit){
 		$instance = isset($this)?$this:self::getCallingClass();
 		$instance->query .= "LIMIT ".$limit;
-	}
-	public function belongsTo($className,$column = null){
-		$ltableName = strtolower($className);
-		if(isset($column)){
-			return $className::findOrFail($this->$column);
-		}else{
-			$index = $ltableName. "_id";
-			return $className::where('id', $this->$index)->get()[0];
-		}
-	}
-	public function hasMany($className){
-		$ltableName = strtolower(get_called_class());
-		return $className::where($ltableName."_id", $this->id)->get();
-	}
-	public function findOrFail($id){
-		$instance = isset($this)?$this:self::getCallingClass();
-		return $instance->where('id',$id)->get()[0];
 	}
 	public function get(){
 		$instance = isset($this)?$this:self::getCallingClass();
@@ -92,5 +88,22 @@ class Model{
 			array_push($arr, $newInstance);
 		}
 		return $arr;
+	}
+	public function findOrFail($id){
+		$instance = isset($this)?$this:self::getCallingClass();
+		return $instance->where('id',$id)->get()[0];
+	}
+	public function belongsTo($className,$column = null){
+		$ltableName = strtolower($className);
+		if(isset($column)){
+			return $className::findOrFail($this->$column);
+		}else{
+			$index = $ltableName. "_id";
+			return $className::where('id', $this->$index)->get()[0];
+		}
+	}
+	public function hasMany($className){
+		$ltableName = strtolower(get_called_class());
+		return $className::where($ltableName."_id", $this->id)->get();
 	}
 }
--- example.php
+++ example.php
@@ -1,13 +1,27 @@
 <?php 
 include 'Model.php';
 	class User extends Model{
+		function __construct(){
+			parent::__construct();
+		}
 		protected $table = "users";
+
+		public function todos(){
+			return $this->hasMany("Todo");
+		}
+	}
+	class Todo extends Model{
+		protected $table = "todo";
+
+		public function user(){
+			return $this->belongsTo('User');
+		}
 	}
 	function wrap($obj){
 		echo "<pre>";
 		print_r($obj);
 		echo "</pre>";
 	}
-	$users = User::all()->get();
-	wrap($users);
+	$user = User::findOrFail(1);
+	wrap($user->todos);
 ?>
